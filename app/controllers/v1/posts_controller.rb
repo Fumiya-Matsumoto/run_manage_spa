dfc1a1fbef7fcdbe8868f2456b0d34a7b9ace44b
Fumiya-Matsumoto <ekiden238@gmail.com>
2022-05-24 22:33:00 +0900

    Modify posts_controller

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 module V1
     class PostsController < ApplicationController
         before_action :set_user
-        def index
+        def index # 特定ユーザーのpost一覧を取得
             user = User.find(params[:user_id])
             posts = user.posts
 
@@ -18,16 +18,39 @@ module V1
             post = @user.posts.build(post_params)
             post.save!
 
-            # 距離の単位をmに直す & postあたりの合計距離を計算・保存
+            
             kind_of_practice = post.kind_of_practice
             post_records = post.post_records
+
             post_distance = 0
+            main_distance = 0
+            recovery_distance = 0
+
             post_records.each do |record|
+                record_type = record.record_type
+                # 距離の単位をmに直す
                 record.distance = align_distance_unit(kind_of_practice, record.distance.to_f) # to_fによりnilを0.0に変換
+                # postあたりの合計距離を計算・保存
                 post_distance += record.distance
+                # paceを計算
+                record.pace = record.time / record.distance # 単位はm/s
+
             end
-            post_distance += post.warming_up_distance.to_f + post.cooling_down_distance.to_f
+            post_distance += post.warming_up_distance.to_f*1000 + post.cooling_down_distance.to_f*1000 # アップとダウンはkm表示という仮定
             post.distance = post_distance
+            
+            main_records = post_records.where(record_type: 0)
+            recovery_records = post_records.where(record_type: 1)
+            num_main = main_records.count
+            num_recovery = recovery_records.count
+            pace_main = main_records.sum(:pace) / num_main.to_f
+            pace_recovery = recovery_records.sum(:pace) / num_recovery.to_f
+            
+            post.num_main = num_main
+            post.num_recovery = num_recovery
+            post.pace_main = pace_main
+            post.pace_recovery = pace_recovery
+
             post.save!
         end
 
@@ -39,9 +62,9 @@ module V1
 
         def post_params
             post_params = params.require(:post).permit(
-                :weather, :place, :kind_of_practice, :strength, :content, :practice_day, :practice_timezone,
+                :weather, :place, :kind_of_practice, :strength, :content, :practice_day, :practice_timezone, :warming_up_distance, :cooling_down_distance,
                 post_records_attributes:[:distance, :time, :pace, :record_type]
-                )
+            )
         end
 
         def align_distance_unit(kind_of_practice, distance) # distanceの単位をmに揃える
@@ -52,14 +75,6 @@ module V1
             else
                 return distance*1000
             end
-        end
-
-        def shape_time(hour: 0, min: 0, sec: 0, msec: 0) 
-            return hour*3600 + min*60 + sec + msec*0.01
-        end
-
-        def calculate_pace(time, distance)
-            return time / distance
         end
     end
 end
