523abe5d29d03a465aa24ce0fc7ef6f8dd2bb595
Fumiya-Matsumoto <ekiden238@gmail.com>
2022-06-12 19:04:01 +0900

    Modify user end point #7

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   namespace :v1 do
     resources :posts
-    mount_devise_token_auth_for 'User', at: 'auth'
-    namespace :auth do
-      get 'sessions', to: "sessions#index"
-    end
-    resources :users do
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations: 'v1/auth/registrations'
+    }
+    # namespace :auth do
+    #   get 'sessions', to: "sessions#index"
+    # end
+    resources :users, only: [:index, :show] do
       get 'posts', to: "posts#user_index"
       resources :best_times
       resources :objective
