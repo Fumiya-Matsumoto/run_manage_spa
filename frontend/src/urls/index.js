e1d51bd9650265a7bea61c90db9764f2338212a6
Fumiya-Matsumoto <ekiden238@gmail.com>
2022-06-24 01:23:24 +0900

    add urls to frontend #18

---
+++ frontend/src/urls/index.js
@@ -0,0 +1,5 @@
+const DEFAULT_API_LOCALHOST = 'http://localhost:3000/v1'
+
+export const usersIndex = `${DEFAULT_API_LOCALHOST}/users`
+export const postsIndex = `${DEFAULT_API_LOCALHOST}/posts`
+export const bestTimesFoods = `${DEFAULT_API_LOCALHOST}/best_times`;
